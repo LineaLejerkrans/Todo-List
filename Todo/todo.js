5b40e158a5e8793271e1d5fe150859d61b4f8e11
LineaLejerkrans <linlej020411@student.jenseneducation.se>
2021-05-06 08:58:41 +0200

    Add files via upload

--- Todo/todo.js
+++ Todo/todo.js
@@ -3,8 +3,10 @@ let input = document.querySelector('.todo_input');
 let list = document.querySelector('.todo_list');
 let todoDiv;
 let todoLi;
+let close;
+let done;
  
-button.addEventListener("click", add)
+button.addEventListener("click", add);
  
 function add(event){
    event.preventDefault(); // otherwise the button will reset
@@ -14,13 +16,27 @@ function add(event){
  
    todoLi = document.createElement('li'); // creates a list
    todoLi.classList.add('todo_item'); // gives it a class
- 
+   
+   close = document.createElement('div');
+   close.classList.add('delete_btn');
+
+   done = document.createElement('div');
+   done.classList.add('complete_btn');
+
    todoLi.appendChild(document.createTextNode(input.value)) // creates a text node with what you wrote in the box than appends it to the list
    todoDiv.appendChild(todoLi); // append the item to the div               
    list.appendChild(todoDiv); // append the div to the list
+   todoDiv.appendChild(close);
+   todoDiv.appendChild(done);
  
    document.getElementById("form").reset(); // after you add a task the text will disappear in the box
    }
+}
+
+   close.addEventListener("click", red);
+
+function red(){
+   console.log("hej");
 }
  
 
