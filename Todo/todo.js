81de2bdce0a0ad8d7e89cd3dd3849a465f9198a2
LineaLejerkrans <linlej020411@student.jenseneducation.se>
2021-05-25 21:50:24 +0200

    Add files via upload

--- Todo/todo.js
+++ Todo/todo.js
@@ -1,42 +1,69 @@
 let button = document.querySelector('.todo_button');
 let input = document.querySelector('.todo_input');
 let list = document.querySelector('.todo_list');
+
+ 
 let todoDiv;
 let todoLi;
 let close;
 let done;
+let n = 0;
  
 button.addEventListener("click", add);
  
 function add(event){
-   event.preventDefault(); // otherwise the button will reset
-   if(input.value){ // if you haven't written anything it will not print it
-   todoDiv = document.createElement('div'); // this creates a div that is called todoDiv
-   todoDiv.classList.add('todo'); // and you give that div this class
- 
-   todoLi = document.createElement('li'); // creates a list
-   todoLi.classList.add('todo_item'); // gives it a class
-   
-   close = document.createElement('div');
-   close.classList.add('delete_btn');
+    event.preventDefault(); // otherwise the button will reset
+    if(input.value){ // if you haven't written anything it will not print it
+        todoDiv = document.createElement('div'); // this creates a div that is called todoDiv
+        todoDiv.classList.add('todo'); // and you give that div this class
+        todoDiv.setAttribute("completed", "false"); // sätt ett värde som säger om upgiften är klar eller inte 
+        todoDiv.id = ++n; // sätter ett id på varje uppgift 
+        console.log(todoDiv.id)
+        
+        todoLi = document.createElement('li'); // creates a list
+        todoLi.classList.add('todo_item'); // gives it a class
 
-   done = document.createElement('div');
-   done.classList.add('complete_btn');
+        todoLi.appendChild(document.createTextNode(input.value)) // creates a text node with what you wrote in the box than appends it to the list
+        todoDiv.appendChild(todoLi); // append the item to the div               
+        list.appendChild(todoDiv); // append the div to the list
+        
+        
+        document.getElementById("form").reset(); // after you add a task the text will disappear in the box
+            
+        close = document.createElement('button');
+        close.classList.add('delete_btn');
+        close.addEventListener("click", delElem); // kan inte skicka med id:et
+  
 
-   todoLi.appendChild(document.createTextNode(input.value)) // creates a text node with what you wrote in the box than appends it to the list
-   todoDiv.appendChild(todoLi); // append the item to the div               
-   list.appendChild(todoDiv); // append the div to the list
-   todoDiv.appendChild(close);
-   todoDiv.appendChild(done);
- 
-   document.getElementById("form").reset(); // after you add a task the text will disappear in the box
-   }
+        done = document.createElement('button');
+        done.classList.add('complete_btn'); 
+        done.addEventListener("click", comElem); // kan inte skicka med id:et
+        
+        todoDiv.appendChild(close);
+        todoDiv.appendChild(done);
+
+    }
 }
 
-   close.addEventListener("click", red);
-
-function red(){
-   console.log("hej");
+function delElem(event){ // event.target tillhör kanppen man tryckte på 
+    event.preventDefault(); // otherwise the button will reset
+    let elem = document.getElementById(event.target.parentNode.id); // hitta id:et på förälden till kanppen som är diven som skas ta bort 
+    elem.parentNode.removeChild(elem); // tar bort diven från föräldern, barnet säger till föräldern att ta bort sig själv 
+    
 }
- 
 
+function comElem(event){
+    event.preventDefault(); // otherwise the button will reset
+    let elem = document.getElementById(event.target.parentNode.id); // hitta id:et på förälden till kanppen som är diven som skas ta bort 
+    console.log(elem.getAttribute("completed"))
+    if(elem.getAttribute("completed") == "true"){ // om uppgiften är avklarad ska den bli oklarad ifall man trycker på klar knappen igen
+        elem.setAttribute("completed", "false");
+        elem.classList.remove('completedItem'); // tar bort css:en för det avklarde uppgifterna
+    }
+    else{
+        elem.setAttribute("completed", "true"); // är den inte klarad men blir det ska den sättas till klar 
+        elem.classList.add('completedItem'); // och få css:en för en klar uppgift 
+    }
+    
+
+}
